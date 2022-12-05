52c329d5980a962ebc4051d4f1a2f930b0e17640
why-002 <115664954+why-002@users.noreply.github.com>
2022-12-05 17:15:04 -0600

    Part 2 complete

--- advent5.py
+++ advent5.py
@@ -42,3 +42,26 @@ with open('input5.txt', 'r') as fp:
         for moved in range(num_moved):
             stacklist[stack_to].objects.append(stacklist[stack_from].objects.pop())
 print(''.join([i.objects[-1] for i in stacklist]))
+
+
+stacklist = [Stack() for _ in range(9)]
+stacklist = original_setup_reader('input5.txt', 8, stacklist)
+
+with open('input5.txt', 'r') as fp:
+    first_char = ''
+    while first_char != 'm':
+        line = fp.readline()
+        first_char = line[0]
+    file = [line] + fp.readlines()
+
+    for i in file:
+        nums = []
+        for char in i:
+            if char.isdigit():
+                nums += char
+        stack_from, stack_to = int(nums.pop(-2))-1, int(nums.pop(-1))-1
+        num_moved = int(f'{nums[0]}{nums[1]}') if len(nums) > 1 else int(nums[0])
+
+        for moved in range(num_moved,0,-1):
+            stacklist[stack_to].objects.append(stacklist[stack_from].objects.pop(-moved))
+print(''.join([i.objects[-1] for i in stacklist]))
