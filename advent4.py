6e14f33bbbe14ddbeeec6b500fc05da772668faa
why-002 <115664954+why-002@users.noreply.github.com>
2022-12-05 12:07:58 -0600

    Both cases for advent4.py are now included

--- advent4.py
+++ advent4.py
@@ -4,6 +4,19 @@ with open("input4.txt", 'r') as fp:
         ij, kl = line.split(',')
         i, j = ij.split('-')
         k, l = kl.split('-')
+        if int(i) <= int(k) <= int(l) <= int(j):
+            count += 1
+        elif int(k) <= int(i) <= int(j) <= int(l):
+            count += 1
+print(count)
+
+
+with open("input4.txt", 'r') as fp:
+    count = 0
+    for line in fp:
+        ij, kl = line.split(',')
+        i, j = ij.split('-')
+        k, l = kl.split('-')
         if int(i) <= int(k) <= int(j):
             count += 1
         elif int(k) <= int(i) <= int(l):
