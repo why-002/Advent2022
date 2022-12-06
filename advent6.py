2a3a152e4cfc89c39645a08ea96bf3c62f18386f
why-002 <115664954+why-002@users.noreply.github.com>
2022-12-06 15:17:56 -0600

    Removed accidental extra iterator that iterated pointlessly through the members of potential_key

--- advent6.py
+++ advent6.py
@@ -10,9 +10,8 @@ def is_unique(string):
 def find_key(file):
     for index, char in enumerate(file):
         potential_key = file[index:index + 4]
-        for i in potential_key:
-            if is_unique(potential_key):
-                return index+4
+        if is_unique(potential_key):
+            return index+4
 
 def find_start_of_message(file):
     for index, char in enumerate(file):
