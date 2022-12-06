9d1020bf528251c8d378abb1870b2e7b6d691954
why-002 <115664954+why-002@users.noreply.github.com>
2022-12-06 15:05:31 -0600

    Advent day 6 part 1 complete

---
+++ advent6.py
@@ -0,0 +1,18 @@
+with open('input6.txt', 'r') as fp:
+    file = fp.readlines()
+    file = file[0]
+
+
+def is_unique(string):
+    return len({i for i in string}) == len(string)
+
+
+def find_key(file):
+    for index, char in enumerate(file):
+        potential_key = file[index:index + 4]
+        for i in potential_key:
+            if is_unique(potential_key):
+                return index+4
+
+
+print(find_key(file))
