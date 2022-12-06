5c4fffd9bfa6854e80db966799229a93c08eff1e
why-002 <115664954+why-002@users.noreply.github.com>
2022-12-06 15:14:55 -0600

    Advent day 6 complete

--- advent6.py
+++ advent6.py
@@ -14,5 +14,13 @@ def find_key(file):
             if is_unique(potential_key):
                 return index+4
 
+def find_start_of_message(file):
+    for index, char in enumerate(file):
+        potential_key = file[index:index + 14]
+        for i in potential_key:
+            if is_unique(potential_key):
+                return index+14
+
 
 print(find_key(file))
+print(find_start_of_message(file))
