e8c75a4d028966d6f703b4e179ac4162ec800bd2
why-002 <115664954+why-002@users.noreply.github.com>
2022-12-12 18:24:30 -0600

    advent11.py finished second part. Had to implement a new number class to keep track of mod values

--- advent11.py
+++ advent11.py
@@ -1,3 +1,44 @@
+class ModNum:
+    def __init__(self, num):
+        self.initial = num
+        self.mod_19 = num % 19
+        self.mod_17 = num % 17
+        self.mod_13 = num % 13
+        self.mod_11 = num % 11
+        self.mod_7 = num % 7
+        self.mod_5 = num % 5
+        self.mod_3 = num % 3
+        self.mod_2 = num % 2
+        self.mod_23 = num % 23
+        self.nums = [self.mod_19, self.mod_17, self.mod_13,
+                     self.mod_11, self.mod_7, self.mod_5,
+                     self.mod_3, self.mod_2, self.mod_23]
+        self.factors = [19, 17, 13, 11, 7, 5, 3, 2, 23]
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            for index, num in enumerate(self.nums):
+                self.nums[index] *= other
+                self.nums[index] %= self.factors[index]
+
+        elif isinstance(other, ModNum):
+            for index, num in enumerate(self.nums):
+                self.nums[index] *= other.nums[index]
+                self.nums[index] %= self.factors[index]
+        return self
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            for index, num in enumerate(self.nums):
+                self.nums[index] += other
+                self.nums[index] %= self.factors[index]
+        return self
+
+    def __mod__(self, other):
+        index = self.factors.index(other)
+        return self.nums[index] % other
+
+
 class Monkey:
     monkey_list = []
 
@@ -21,6 +62,7 @@ class Monkey:
 
     def pass_to_monkey(self):
         while self.items:
+
             if self.items[0] % self.test == 0:
                 for i in Monkey.monkey_list:
                     if str(self.passed) in i.name:
@@ -36,9 +78,11 @@ class Monkey:
             self.inspect_log += 1
             if isinstance(self.operation_value, str):
                 item = self.choices[self.operation](item, item)
+                # item = item // 3
                 new_items.append(item)
             else:
                 item = self.choices[self.operation](item, self.operation_value)
+                # item = item // 3
                 new_items.append(item)
         self.items = new_items
 
@@ -51,6 +95,8 @@ with open('input11.txt', 'r') as fp:
             items = fp.readline().strip().split(':')
             items = items[1].strip().split(', ')
             items = map(int, items)
+            items = list(items)
+            items = map(ModNum, items)
             new_monkey.items = list(items)
 
             operation = fp.readline().strip().split(' ')
@@ -67,11 +113,10 @@ with open('input11.txt', 'r') as fp:
             new_monkey.failed = int(failed)
 
 for index in range(10000):
-    print(index)
     for i in Monkey.monkey_list:
         i.inspect()
         i.pass_to_monkey()
 print(Monkey.monkey_list)
 print([i.items for i in Monkey.monkey_list])
 Monkey.monkey_list.sort()
-print(Monkey.monkey_list[-1].inspect_log*Monkey.monkey_list[-2].inspect_log)
+print(Monkey.monkey_list[-1].inspect_log * Monkey.monkey_list[-2].inspect_log)
