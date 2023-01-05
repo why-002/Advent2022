ef47d04ff8d6df18b7416d13b0cfda954c80e696
why-002 <115664954+why-002@users.noreply.github.com>
2023-01-04 22:22:13 -0600

    rewrote the implementation of Block's __getitem__() and the find_sides function
    this was done to remove the redundant copy of the find_sides implementation in the remove_pocket_score function

--- advent18.py
+++ advent18.py
@@ -8,21 +8,24 @@ def interpret_file(fname):
             blocks.append(line)
     return blocks
 
-def find_sides(blocks):
+
+def find_sides(blocks, checks=None):
+    if checks is None:
+        checks = blocks
     total_sides = 0
-    for line in blocks:
+    for block in blocks:
         sides = 6
-        for b in blocks:
-            x_match = b[0] == line[0]
-            y_match = b[1] == line[1]
-            z_match = b[2] == line[2]
+        for check in checks:
+            x_match = block[0] == check[0]
+            y_match = block[1] == check[1]
+            z_match = block[2] == check[2]
             matches = [x_match, y_match, z_match]
-            if matches.count(True) == 2 and b != line:
-                if x_match is False and abs(b[0] - line[0]) == 1:
+            if matches.count(True) == 2 and block != check:
+                if x_match is False and abs(block[0] - check[0]) == 1:
                     sides -= 1
-                elif y_match is False and abs(b[1] - line[1]) == 1:
+                elif y_match is False and abs(block[1] - check[1]) == 1:
                     sides -= 1
-                elif z_match is False and abs(b[2] - line[2]) == 1:
+                elif z_match is False and abs(block[2] - check[2]) == 1:
                     sides -= 1
         total_sides += sides
     return total_sides
@@ -38,52 +41,47 @@ class Block:
     def __lt__(self, other):
         return self.distance < other.distance
 
+    def __getitem__(self, item):
+        if item == 0:
+            return self.x
+        if item == 1:
+            return self.y
+        if item == 2:
+            return self.z
+        raise IndexError
+
 
 def remove_pocket_score(blocks):
     steps = set()
     x_vals = [i[0] for i in blocks]
     y_vals = [i[1] for i in blocks]
     z_vals = [i[2] for i in blocks]
-    for x in range(max(x_vals)+1):
-        for y in range(max(y_vals)+1):
-            for z in range(max(z_vals)+1):
-                if (x,y,z) not in blocks:
-                    steps.add(Block((x,y,z)))
+    for x in range(max(x_vals) + 1):
+        for y in range(max(y_vals) + 1):
+            for z in range(max(z_vals) + 1):
+                if (x, y, z) not in blocks:
+                    steps.add(Block((x, y, z)))
     pockets = set()
     while steps:
         if min(steps).distance == float('inf'):
-            pockets = steps.copy()
-            break
-        active = min(steps)
-        for i in steps:
-            x_distance = abs(active.x-i.x)
-            y_distance = abs(active.y-i.y)
-            z_distance = abs(active.z-i.z)
-            distances = [x_distance, y_distance, z_distance]
-            if distances.count(0) == 2:
-                try:
-                    distances.index(1)
-                    i.distance = active.distance + 1
-                except ValueError:
-                    pass
-        steps.remove(active)
-    checks = blocks + [(i.x, i.y, i.z) for i in pockets]
-    current_score = 0
-    for b in blocks:
-        sides = 6
-        for c in checks:
-            x_match = c[0] == b[0]
-            y_match = c[1] == b[1]
-            z_match = c[2] == b[2]
-            matches = [x_match, y_match, z_match]
-            if matches.count(True) == 2:
-                if x_match is False and abs(c[0] - b[0]) == 1:
-                    current_score -= 1
-                elif y_match is False and abs(c[1] - b[1]) == 1:
-                    current_score -= 1
-                elif z_match is False and abs(c[2] - b[2]) == 1:
-                    current_score -= 1
-        current_score += sides
+            pockets = [i for i in steps]
+            steps = False
+        else:
+            active = min(steps)
+            for i in steps:
+                x_distance = abs(active.x - i.x)
+                y_distance = abs(active.y - i.y)
+                z_distance = abs(active.z - i.z)
+                distances = [x_distance, y_distance, z_distance]
+                if distances.count(0) == 2:
+                    try:
+                        distances.index(1)
+                        i.distance = active.distance + 1 if active.distance + 1 < i.distance else i.distance
+                    except ValueError:
+                        pass
+            steps.remove(active)
+    checks = blocks + pockets
+    current_score = find_sides(blocks, checks)
     return current_score
 
 
